65c8c1c0ffbcc85f9c71890d12a1b120713e9e1c
Dmitrii Fediuk <dfediuk@gmail.com>
2025-10-20 00:48:43 +0100

    refactoring

--- Model/Client/Classic.php
+++ Model/Client/Classic.php
@@ -1,14 +1,13 @@
 <?php
 namespace Dfe\Alignet\Model\Client;
+use Dfe\Alignet\Model\Client\Classic\Config;
+use Dfe\Alignet\Model\Client\Classic\Order;
 final class Classic extends \Dfe\Alignet\Model\Client {
 	/**
-	 * @param Classic\Config $configHelper
-	 * @param Classic\Order $orderHelper
+	 * @param Config $configHelper
+	 * @param Order $orderHelper
 	 */
-	function __construct(
-		Classic\Config $configHelper,
-		Classic\Order $orderHelper
-	) {
+	function __construct(Config $configHelper, Order $orderHelper) {
 		parent::__construct(
 			$configHelper,
 			$orderHelper
