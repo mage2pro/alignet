ac7be91d87d616dfdd11cb3a25fe6bd3f1024f1f
Dmitrii Fediuk <dfediuk@gmail.com>
2025-10-19 22:31:13 +0100

    https://github.com/innomuebles/m2/issues/53

--- lib/o.php
+++ lib/o.php
@@ -5,4 +5,4 @@ use Dfe\Alignet\Model\Session as S;
  * 2025-09-23 Dmitrii Fediuk https://upwork.com/fl/mage2pro
  * "Refactor the `Alignet_Paymecheckout` module": https://github.com/innomuebles/m2/issues/10
  */
-function dfe_cr_payme_sess() {return df_o(S::class);}
+function dfe_alignet_payme_sess() {return df_o(S::class);}
