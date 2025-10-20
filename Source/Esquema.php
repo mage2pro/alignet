4c437e10721b8acfb052dae53d5c5e0a2179ed47
Dmitrii Fediuk <dfediuk@gmail.com>
2025-10-21 00:57:41 +0100

    refactoring

--- Source/Esquema.php
+++ Source/Esquema.php
@@ -1,23 +1,7 @@
 <?php
-/**
- * SquareUp
- *
- * Action Source Model
- *
- * @category    Payme
- * @package     Payme
- * @copyright   2019
- *
- */
-
 namespace Dfe\Alignet\Source;
-
 use Magento\Framework\Option\ArrayInterface;
 use Magento\Payment\Model\Method\AbstractMethod;
-
-/**
- * Class Esquema
- */
 class Esquema implements ArrayInterface
 {
     /**
