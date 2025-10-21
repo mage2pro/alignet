423641e1f5e655a4546cf3289fd502c4cf1059a7
Dmitrii Fediuk <dfediuk@gmail.com>
2025-10-21 21:44:54 +0100

    https://github.com/mage2pro/alignet/issues/2

--- Model/Client/ConfigInterface.php
+++ Model/Client/ConfigInterface.php
@@ -2,18 +2,15 @@
 namespace Dfe\Alignet\Model\Client;
 /**
  * @see \Dfe\Alignet\Model\Client\Classic\Config
- * @see \Dfe\Alignet\Model\Client\Rest\Config
  */
 interface ConfigInterface {
 	/**
 	 * @see \Dfe\Alignet\Model\Client\Classic\Config::getConfig()
-	 * @see \Dfe\Alignet\Model\Client\Rest\Config::getConfig()
 	 */
     function getConfig($key);
 
 	/**
 	 * @see \Dfe\Alignet\Model\Client\Classic\Config::setConfig()
-	 * @see \Dfe\Alignet\Model\Client\Rest\Config::setConfig()
 	 */
 	function setConfig();
 }
