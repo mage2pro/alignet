84d4b60e38347f78412bc7546681c01781758f93
Dmitrii Fediuk <dfediuk@gmail.com>
2025-10-22 06:23:39 +0100

    https://github.com/mage2pro/alignet/issues/11

--- Cfg.php
+++ Cfg.php
@@ -35,7 +35,7 @@ final class Cfg {
 	 * @used-by self::s()
 	 */
 	private function __construct() {
-		$this->payme_entorno = $this->scopeConfig->getValue('payment/payme_gateway/main_parameters/payme_environment', \Magento\Store\Model\ScopeInterface::SCOPE_STORE);
+		$this->payme_entorno = df_cfg('payment/payme_gateway/main_parameters/payme_environment', \Magento\Store\Model\ScopeInterface::SCOPE_STORE);
 		switch ($this->payme_entorno) {
 			case 0:
 				$this->wsdomain = 'https://integracion.alignetsac.com';
