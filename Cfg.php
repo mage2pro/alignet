346129493bdcd7f30a8c5903402af0709c97fcba
Dmitrii Fediuk <dfediuk@gmail.com>
2025-10-22 06:32:21 +0100

    https://github.com/mage2pro/alignet/issues/11

--- Cfg.php
+++ Cfg.php
@@ -69,7 +69,7 @@ final class Cfg {
 		if ($payme_wallet_secret) {
 			$this->payme_wallet_secret = $payme_wallet_secret;
 		}
-		$payme_adquir_id_dls = df_cfg(Paymecheckout::XML_PATH_PAYME_ADQUIR_ID_DLS);
+		$payme_adquir_id_dls = df_cfg('payment/payme_gateway/pos_parameters_dolares/payme_adquir_id_dls');
 		if ($payme_adquir_id_dls) {
 			$this->payme_adquir_id_dls = $payme_adquir_id_dls;
 		}
