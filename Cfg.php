8a7f804c5f20c8080e32436c540fa3892130fb34
Dmitrii Fediuk <dfediuk@gmail.com>
2025-10-22 06:33:53 +0100

    https://github.com/mage2pro/alignet/issues/11

--- Cfg.php
+++ Cfg.php
@@ -51,10 +51,7 @@ final class Cfg {
 		}
 		$this->payme_adquir_id = df_cfg('payment/payme_gateway/pos_parameters_soles/payme_adquir_id');
 		$this->payme_comerce_id = df_cfg('payment/payme_gateway/pos_parameters_soles/payme_comerce_id');
-		 $payme_vpos_id = df_cfg('payment/payme_gateway/pos_parameters_soles/payme_vpos_id');
-		if ($payme_vpos_id) {
-			$this->payme_vpos_id = $payme_vpos_id;
-		}
+		$this->payme_vpos_id = df_cfg('payment/payme_gateway/pos_parameters_soles/payme_vpos_id');
 		$payme_wallet_id = df_cfg('payment/payme_gateway/pos_parameters_soles/payme_wallet_id');
 		if ($payme_wallet_id) {
 			$this->payme_wallet_id = $payme_wallet_id;
