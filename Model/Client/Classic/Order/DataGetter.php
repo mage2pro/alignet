5a820b4dc2ca2f47df04446e4ff3890599f206b9
Dmitrii Fediuk <dfediuk@gmail.com>
2025-10-23 05:14:58 +0100

    refactoring

--- Model/Client/Classic/Order/DataGetter.php
+++ Model/Client/Classic/Order/DataGetter.php
@@ -49,8 +49,7 @@ final class DataGetter {
 	function getBasicData(\Magento\Sales\Model\Order $order) {
 		$oid =(int)$order->getId(); /** @var int $oid */
 		$ba = $order->getBillingAddress();
-		$shippingAddress = $order->getShippingAddress();
-		$shippingAddressArray =$shippingAddress->getData();
+		$shippingAddressArray = $order->getShippingAddress()->getData();
 		$taxReturnBase = number_format(($order->getGrandTotal() - $order->getTaxAmount()),2,'.','');
 		if ($order->getTaxAmount() == 0) {
 			$taxReturnBase = 0;
