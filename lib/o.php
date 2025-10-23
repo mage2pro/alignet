905b1230f175650ccf226f2a3335f07acad1f6d8
Dmitrii Fediuk <dfediuk@gmail.com>
2025-10-23 04:51:20 +0100

    https://github.com/mage2pro/alignet/issues/17

--- lib/o.php
+++ lib/o.php
@@ -24,7 +24,6 @@ function dfe_alignet_cl() {return df_new_om(Cl::class);}
  * @used-by \Dfe\Alignet\Model\Client\Classic\Order\DataGetter::getSigForOrderRetrieve()
  * @used-by \Dfe\Alignet\Model\Client\Classic\Order\DataGetter::getTestMode()
  * @used-by \Dfe\Alignet\Model\Client\Classic\Order\DataGetter::userCodePayme()
- * @used-by \Dfe\Alignet\Model\Client\Classic\Order\Notification::getPayuplOrderId()
  * @used-by vendor/mage2pro/alignet/view/frontend/templates/classic/form.phtml
  */
 function dfe_alignet_cfg() {return Cfg::s();}
