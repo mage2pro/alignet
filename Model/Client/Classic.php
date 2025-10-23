666212b64e8cb69f4a4cd5350c47c8fa9085dc7d
Dmitrii Fediuk <dfediuk@gmail.com>
2025-10-23 04:21:14 +0100

    https://github.com/mage2pro/alignet/issues/6

--- Model/Client/Classic.php
+++ Model/Client/Classic.php
@@ -1,6 +1,5 @@
 <?php
 namespace Dfe\Alignet\Model\Client;
-use Dfe\Alignet\Cfg;
 use Dfe\Alignet\Model\Client\Classic\Order;
 /** @used-by dfe_alignet_cl() */
 final class Classic extends \Dfe\Alignet\Model\Client {
